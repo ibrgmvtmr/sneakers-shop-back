e47cad39e24a0f9807b1648265354af42a59b3d0
Timur <storyphase@yandex.com>
2024-03-25 00:50:04 +0600

    feat: add controller

---
+++ src/main/java/edu/alatoo/sneakers/controller/ItemController.java
@@ -0,0 +1,9 @@
+package edu.alatoo.sneakers.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/items")
+public class ItemController {
+}
