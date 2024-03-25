70674540eeea1dcd57c1650d313779af4217ebcb
Timur <storyphase@yandex.com>
2024-03-25 07:22:36 +0600

    feat: add controllers

--- src/main/java/edu/alatoo/sneakers/controller/AuthController.java
+++ src/main/java/edu/alatoo/sneakers/controller/AuthController.java
@@ -1,4 +1,36 @@
 package edu.alatoo.sneakers.controller;
 
+import edu.alatoo.sneakers.payload.LoginRequestDTO;
+import edu.alatoo.sneakers.payload.SignupRequestDTO;
+import edu.alatoo.sneakers.security.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> register(@Valid @RequestBody SignupRequestDTO signUpRequest) {
+        authService.register(signUpRequest);
+        return new ResponseEntity<>("Пользователь успешно зарегестрирован", HttpStatus.OK);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticate(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
+        authService.authenticate(loginRequestDTO);
+        return new ResponseEntity<>("Добро пожаловать в систему!", HttpStatus.OK);
+    }
 }
--- src/main/java/edu/alatoo/sneakers/controller/ItemController.java
+++ src/main/java/edu/alatoo/sneakers/controller/ItemController.java
@@ -1,9 +1,48 @@
 package edu.alatoo.sneakers.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import edu.alatoo.sneakers.model.User;
+import edu.alatoo.sneakers.payload.ItemRequestDTO;
+import edu.alatoo.sneakers.payload.ItemResponseDTO;
+import edu.alatoo.sneakers.service.ItemService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/items")
 public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemResponseDTO> createItem(@AuthenticationPrincipal User user,
+                                                      @RequestBody ItemRequestDTO itemRequestDTO) {
+        ItemResponseDTO createdItem = itemService.create(user, itemRequestDTO);
+        return ResponseEntity.ok(createdItem);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ItemResponseDTO> updateItem(@AuthenticationPrincipal User user,
+                                                      @PathVariable Long id,
+                                                      @RequestBody ItemRequestDTO itemRequestDTO) {
+        ItemResponseDTO updatedItem = itemService.update(user, id, itemRequestDTO);
+        return ResponseEntity.ok(updatedItem);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ItemResponseDTO> getItem(@PathVariable Long id) {
+        ItemResponseDTO item = itemService.get(id);
+        return ResponseEntity.ok(item);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ItemResponseDTO>> getAllItems(@AuthenticationPrincipal User user) {
+        List<ItemResponseDTO> items = itemService.getAll(user.getId());
+        return ResponseEntity.ok(items);
+    }
 }
